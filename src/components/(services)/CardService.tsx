b67f65e8416ddd777646e2f7f823c14a41eb3814
ali <alihgad2@gmail.com>
2024-05-15 15:09:05 +0300

    hover cards

--- src/components/(services)/CardService.tsx
+++ src/components/(services)/CardService.tsx
@@ -1,21 +1,35 @@
-import Image from "next/image";
-import React from "react";
-import pc from "./../../../public/LA icons/LA icons/pc.png";
-import
+"use client";
+import Image, { StaticImageData } from "next/image";
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
 
-export default function CardService({className}:{
-    className?:string,
+export default function CardService({
+  className,
+  src,
+  headline,
+  hover
+}: {
+  className?: string;
+  src: StaticImageData;
+  headline: string;
+  hover?: boolean;
 }) {
   return (
     <>
-      <div className={className}>
-        <div className="rounded-xl card p-10 font-open text-white bg-[#666]">
-          <Image src={pc} alt="pc" />
-          <h6 className="mt-6 text-lg font-extrabold">Website Development</h6>
-          <p className="mt-6">Lorem ipsum dolor sit amet consectetur. Sit lobortis.</p>
-          <hr className="mt-3"/>
+      
+        <div className="group relative z-30">
+          <div className={`${hover ? 'group-hover:bg-[#262523]' : null}  rounded-xl card p-12 font-open text-white bg-[#666]  transition-all duration-500`}>
+            <Image src={src} alt="pc" />
+            <h6 className={`${className} mt-6 text-lg font-extrabold w-2/4 `}>
+              {headline}
+            </h6>
+            <p className="mt-6">
+              Lorem ipsum dolor sit amet consectetur. Sit lobortis.
+            </p>
+            <hr className="mt-3" />
+          </div>
         </div>
-      </div>
     </>
   );
 }
--- src/components/(services)/ServicesCards.tsx
+++ src/components/(services)/ServicesCards.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import CardService from "./CardService";
 import SectionHeading from "../(global)/SectionHeading";
+import pc from "./../../../public/LA icons/LA icons/pc.png";
+import ai from "./../../../public/LA icons/LA icons/ai icon.png";
+import ui from "./../../../public/LA icons/LA icons/ui ux icon.png";
+import mobile from "./../../../public/LA icons/LA icons/mobile.png";
+import { HoverEffect } from "../ui/card-hover-effect";
 
 export default function ServicesCards() {
   return (
@@ -12,11 +17,22 @@ export default function ServicesCards() {
             body="services we are providing to our clients"
           />
           <div className="flex justify-between mt-10 flex-col gap-8 lg:flex-row">
-            <CardService className="lg:pr-6"/>
-            <CardService className="lg:px-6"/>
-            <CardService className="lg:px-6"/>
-            <CardService className="lg:pl-6"/>
+            <CardService hover={true} src={pc} headline="Web Develobment" />
+            <CardService hover={true} src={ai} headline="AI Integration" />
+            <CardService hover={true} className="width" src={mobile} headline="Mobile App Develobment" />
+            <CardService hover={true} src={ui} headline="UI/UX Design" />
           </div>
+          <HoverEffect
+
+            items={[
+            <CardService key={0} src={pc} headline="Web Develobment" />
+            ,
+            <CardService key={2} src={ai} headline="AI Integration" />,
+            <CardService key={1} src={mobile} headline="Mobile App Develobment" className="width" />,
+            <CardService key={3} src={ui} headline="UI/UX Design" />
+                            
+            ]}
+          />
         </div>
       </section>
     </>
